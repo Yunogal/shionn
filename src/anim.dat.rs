a5f2779235d18d11ba0ba0812795f3f919dcb462
yunogasai <yunogasai728728@gmail.com>
2025-08-31 11:28:07 +0100

    anim

--- src/anim.dat.rs
+++ src/anim.dat.rs
@@ -5,6 +5,6 @@ pub use anim_gax::parce;
 #[test]
 fn size() {
     use std::mem::{align_of, size_of};
+    assert_eq!(align_of::<Dat>(), 4);
     assert_eq!(size_of::<Dat>(), 20);
-    assert_eq!(align_of::<Dat>(), 4)
 }
